8b0457c7e9916e98d89ae58b4de1444b902ad17d
Mukhtarov-Ramazan <mukhtarovramazan239@gmail.com>
2024-02-25 22:21:38 +0300

    Сделал минимальную настройку

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<script setup>
+</script>
+
+<template>
+  <div class="MainPage">
+    <h1>Это мой новый тестовый проект по изучению Nuxt3 и TypeScript</h1>
+  </div>
+</template>
+
+
+<style scoped>
+
+</style>
